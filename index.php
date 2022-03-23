94701b9d667fbb16c41988586e26aecb25c1a7ae
Maksim Shlomin <m.shlomin@corp.vk.com>
2022-03-23 21:56:11 +0300

    core communication does not need any more

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 </div>
 
 <div class="text-center">
-<a href="https://oauth.vk.com/authorize?client_id=8085028&redirect_uri=https://duntek.ru/myza/vk_oauth&state=https://duntek.ru/&response_type=code&scope=email">
+<a href="https://oauth.vk.com/authorize?client_id=8085028&redirect_uri=https://duntek.ru/myza/vk_auth&state=https://duntek.ru/&response_type=code&scope=email">
     <button type="button" class="btn btn-primary btn-lg px-4 gap-3">вход через ВКонтакте</button>
 </a>
 </div>
