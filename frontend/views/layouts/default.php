a9e35aeaeef468d920071a640a3e9ad3431f9250
Maksim Shlomin <m.shlomin@corp.vk.com>
2022-04-17 19:15:07 +0300

    ya metrika

--- frontend/views/layouts/default.php
+++ frontend/views/layouts/default.php
@@ -1,29 +1,55 @@
 <?php
 
-/** @var \yii\web\View $this */
-/** @var string $content */
+use yii\web\View;
+
+/**
+ * @var View $this
+ * @var string $content
+ */
+
 ?>
 <?php $this->beginPage() ?>
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript">
+          (function(m, e, t, r, i, k, a) {
+            m[i] = m[i] || function() {(m[i].a = m[i].a || []).push(arguments)};
+            m[i].l = 1 * new Date();
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+          })
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-  <title>криптокотельная</title>
-</head>
-<body>
-<?php $this->beginBody() ?>
-<?= $content ?>
-<?php $this->endBody() ?>
+          ym(88407990, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true
+          });
+        </script>
+        <noscript>
+            <div><img src="https://mc.yandex.ru/watch/88407990" style="position:absolute; left:-9999px;" alt=""/></div>
+        </noscript>
+        <!-- /Yandex.Metrika counter -->
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+              integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+              crossorigin="anonymous">
 
-</body>
-</html>
+        <title>криптокотельная</title>
+    </head>
+    <body>
+    <?php $this->beginBody() ?>
+    <?= $content ?>
+    <?php $this->endBody() ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+            crossorigin="anonymous"></script>
+
+    </body>
+    </html>
 <?php $this->endPage();
