cde9644dca3fe117f4be336587cc521eaa360022
Maksim Shlomin <m.shlomin@corp.vk.com>
2022-04-22 13:58:43 +0300

    vk pixel

--- frontend/views/layouts/default.php
+++ frontend/views/layouts/default.php
@@ -38,6 +38,8 @@ AppAsset::register($this);
         </noscript>
         <!-- /Yandex.Metrika counter -->
 
+        <script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src='https://vk.com/js/api/openapi.js?169',t.onload=function(){VK.Retargeting.Init("VK-RTRG-1366857-aWgPP"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-1366857-aWgPP" style="position:fixed; left:-999px;" alt=""/></noscript>
+
         <?php $this->head(); ?>
 
         <title>криптокотельная duntek</title>
