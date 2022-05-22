7f7b75c66857780873a70f9ae100faa169ad9daa
Maksim Shlomin <m.shlomin@corp.vk.com>
2022-05-22 12:34:11 +0300

    admin has started

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -12,93 +12,87 @@ use yii\web\Response;
 /**
  * Site controller
  */
-class SiteController extends Controller
-{
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'rules' => [
-                    [
-                        'actions' => ['login', 'error'],
-                        'allow' => true,
-                    ],
-                    [
-                        'actions' => ['logout', 'index'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
+class SiteController extends Controller {
+  /**
+   * {@inheritdoc}
+   */
+  public function behaviors() {
+    return [
+      'access' => [
+        'class' => AccessControl::className(),
+        'rules' => [
+          [
+            'actions' => ['login', 'error'],
+            'allow'   => true,
+          ],
+          [
+            'actions' => ['logout', 'index'],
+            'allow'   => true,
+            'ips'     => Yii::$app->params['adminIPs'],
+          ],
+        ],
+      ],
+      'verbs'  => [
+        'class'   => VerbFilter::className(),
+        'actions' => [
+          'logout' => ['post'],
+        ],
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function actions() {
+    return [
+      'error' => [
+        'class' => 'yii\web\ErrorAction',
+      ],
+    ];
+  }
+
+  /**
+   * Displays homepage.
+   *
+   * @return string
+   */
+  public function actionIndex() {
+    return $this->render('index');
+  }
+
+  /**
+   * Login action.
+   *
+   * @return string|Response
+   */
+  public function actionLogin() {
+    if (!Yii::$app->user->isGuest) {
+      return $this->goHome();
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-        ];
+    $this->layout = 'blank';
+
+    $model = new LoginForm();
+    if ($model->load(Yii::$app->request->post()) && $model->login()) {
+      return $this->goBack();
     }
 
-    /**
-     * Displays homepage.
-     *
-     * @return string
-     */
-    public function actionIndex()
-    {
-        return $this->render('index');
-    }
+    $model->password = '';
 
-    /**
-     * Login action.
-     *
-     * @return string|Response
-     */
-    public function actionLogin()
-    {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
+    return $this->render('login', [
+      'model' => $model,
+    ]);
+  }
 
-        $this->layout = 'blank';
+  /**
+   * Logout action.
+   *
+   * @return Response
+   */
+  public function actionLogout() {
+    Yii::$app->user->logout();
 
-        $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
-        }
-
-        $model->password = '';
-
-        return $this->render('login', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Logout action.
-     *
-     * @return Response
-     */
-    public function actionLogout()
-    {
-        Yii::$app->user->logout();
-
-        return $this->goHome();
-    }
+    return $this->goHome();
+  }
 }
--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -4,11 +4,7 @@
 /** @var string $content */
 
 use backend\assets\AppAsset;
-use common\widgets\Alert;
-use yii\bootstrap4\Breadcrumbs;
-use yii\bootstrap4\Html;
-use yii\bootstrap4\Nav;
-use yii\bootstrap4\NavBar;
+use yii\bootstrap5\Html;
 
 AppAsset::register($this);
 ?>
@@ -26,52 +22,17 @@ AppAsset::register($this);
 <?php $this->beginBody() ?>
 
 <header>
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post', ['class' => 'form-inline'])
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
-    }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
-    ?>
+
 </header>
 
 <main role="main" class="flex-shrink-0">
     <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
         <?= $content ?>
     </div>
 </main>
 
 <footer class="footer mt-auto py-3 text-muted">
-    <div class="container">
-        <p class="float-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-        <p class="float-right"><?= Yii::powered() ?></p>
-    </div>
+
 </footer>
 
 <?php $this->endBody() ?>
